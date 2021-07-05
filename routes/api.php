5a96564aa5d747a10a06c409d405206ac54b3399
furiousnur <furiousnur@gmail.com>
2021-07-06 00:23:00 +0600

    test

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::resource('category',CategoryController::class);
 Route::resource('product',ProductController::class);
